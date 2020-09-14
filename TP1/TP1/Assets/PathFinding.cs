a551f66c59e65b5180a2546c1bbb0d806e039994
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-14 14:37:06 +0200

    TP1 & TP2

---
+++ TP1/TP1/Assets/PathFinding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour
+{
+
+    // -17 / 7 à 16 / 6
+    // inclus !
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TP2/Boids.md
@@ -0,0 +1,5 @@
+A faire et à rendre :
+
+- Ajouter des obstacles voire des murs dans le programme boids_balls.py
+- Ajouter une méthode pour les boids aillent vers une cible
+- Ajouter un participant contrôlé au clavier qui sera la cible
