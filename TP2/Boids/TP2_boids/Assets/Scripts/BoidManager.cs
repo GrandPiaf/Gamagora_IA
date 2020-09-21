87d429e2e06ec3fec244d56d7cf9f35f62a99674
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-21 13:43:07 +0200

    Boid prefab, scripts & scene created Boid instantiation added in script

---
+++ TP2/Boids/TP2_boids/Assets/Scripts/BoidManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour
+{
+    [Range(10, 100)]
+    public int boidNumber;
+
+    public GameObject boidPrefab;
+
+    public GameObject cube;
+
+    // Boid list
+    List<Boid> boids;
+
+    void Start()
+    {
+        boids = new List<Boid>(boidNumber);
+
+        for (int i = 0; i < boidNumber; ++i) {
+
+            float x = 0;
+            float y = 0;
+            float z = 0;
+            
+            Instantiate(boidPrefab, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+}
