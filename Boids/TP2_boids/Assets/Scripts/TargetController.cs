02cec85ceef24782a2a09547a22074d81207d222
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-10-15 12:17:01 +0200

    Move projects & small changes in FSM

---
+++ Boids/TP2_boids/Assets/Scripts/TargetController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    [Range(1, 100)]
+    public float speed = 2f;
+
+    public BoidManager manager;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Z)) {
+            movement.z = 1;
+        } else if(Input.GetKey(KeyCode.S)){
+            movement.z = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            movement.x = 1;
+        } else if (Input.GetKey(KeyCode.Q)) {
+            movement.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            movement.y = 1;
+        } else if (Input.GetKey(KeyCode.LeftControl)) {
+            movement.y = -1;
+        }
+
+        movement.Normalize();
+
+        float borderX = manager.cube.transform.localScale.x / 2 * 0.9f;
+        float borderY = manager.cube.transform.localScale.y / 2 * 0.9f;
+        float borderZ = manager.cube.transform.localScale.z / 2 * 0.9f;
+
+        if (transform.position.x < -borderX && movement.x < 0) {
+            movement.x = 0;
+        }
+        if (transform.position.y < -borderY && movement.y < 0) {
+            movement.y = 0;
+        }
+        if (transform.position.z < -borderZ && movement.z < 0) {
+            movement.z = 0;
+        }
+
+        if (transform.position.x > borderX && movement.x > 0) {
+            movement.x = 0;
+        }
+        if (transform.position.y > borderY && movement.y > 0) {
+            movement.y = 0;
+        }
+        if (transform.position.z > borderZ && movement.z > 0) {
+            movement.z = 0;
+        }
+
+        rb.MovePosition(transform.position + movement * Time.deltaTime * speed);
+
+    }
+}
---
+++ FSM/Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    public PathGrid grid;
+
+    public float movementTimer = 0;
+    public float movementDelay = 2.0f;
+
+
+    /** FSM **/
+    internal PlayerState currentState = PlayerState.Move;
+
+
+    void Update() {
+
+        FSM();
+
+
+
+        int horizontal = (int)Input.GetAxisRaw("Horizontal");
+        int vertical = (int)Input.GetAxisRaw("Vertical");
+
+        // if the game doesn't allow diagonal movements, we force horizontol movement
+        if (!grid.allowDiagonals && vertical != 0 && horizontal != 0) {
+            vertical = 0;
+        }
+
+        movementTimer += Time.deltaTime;
+
+        // Cannot move until delay is finished
+        if (movementDelay > movementTimer) {
+            return;
+        }
+
+        // Can move
+        movementTimer -= movementDelay;
+
+
+        Vector3 nextPosition = transform.position + new Vector3(horizontal, vertical);
+        Node nextNode = grid.NodeFromWorldPoint(nextPosition);
+        
+        // We cannot move there, skipping movement
+        if (nextNode.isSolid) {
+            return;
+        }
+
+        // Else, moving
+        transform.position = nextPosition;
+
+    }
+
+    // FSM Management
+    void FSM() {
+        // 2 states :
+        //  - Move : The player move around the game & can be eaten by the frog -> in this case it loose
+        //  - Eat : The player move around & can eat the frog -> In this case, it wins
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+
+        GameObject go = collision.gameObject;
+
+        // If it is an enemy
+        if (go.GetComponent<AStarEnemy>() != null) {
+            switch (currentState) {
+                case PlayerState.Move:
+                    Debug.Log("PLAYER LOSE");
+                    Destroy(gameObject);
+                    break;
+                case PlayerState.Eat:
+                    Debug.Log("ENEMY LOSE");
+                    Destroy(go);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //If it is a pastille
+        if (go.GetComponent<Pastille>() != null) {
+            currentState = PlayerState.Eat;
+            Destroy(go);
+        }
+
+        // Else, we don't know what type is the collision
+        // Do nothing
+    }
+}
